ebe1182a71440ec9a4b3748470c783c35dc4b707
Spidy-2 <dhingrashridhar@gmail.com>
2025-07-02 22:17:30 +0530

    FoodDeliveryApp

--- app/src/main/java/com/example/fooddeliveryapp/FoodItemAdapter.java
+++ app/src/main/java/com/example/fooddeliveryapp/FoodItemAdapter.java
@@ -16,6 +16,7 @@ public class FoodItemAdapter extends RecyclerView.Adapter<FoodItemAdapter.FoodIt
     private final Context context;
     private final List<FoodItem> foodItemList;
 
+
     public FoodItemAdapter(Context context, List<FoodItem> foodItemList) {
         this.context = context;
         this.foodItemList = foodItemList;
